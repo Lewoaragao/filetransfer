403309840117a8abc1ec0eb942249aa6705d635a
Leonardo Aragão <lewoaragao@gmail.com>
2023-11-14 08:44:42 -0300

    [refactor] organize imports

--- src/main/java/com/filetransfer/controller/PartController.java
+++ src/main/java/com/filetransfer/controller/PartController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.Part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
